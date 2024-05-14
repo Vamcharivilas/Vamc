26e1759f5f0525534dc59cbf570e5fbab5e72e01
Vamcharivilas <vamshiharivilas1112@gmail.com>
2024-05-14 13:12:21 +0530

    lab1

---
+++ Swaptoadd.java
@@ -0,0 +1,21 @@
+package sample;
+
+public class Swaptoadd {
+
+	public static void main(String[] args) {
+	int a=10;
+	int b=20;
+	System.out.println("before swaping values"+a);
+	System.out.println("before swaping values"+b);
+	int temp=0;
+	temp=a;
+	a=b;
+    b=temp;
+	System.out.println("before swaping values"+a);
+	System.out.println("before swaping values"+b);
+	temp=a+b;
+	System.out.println("before swaping values"+temp);
+
+	}
+
+}
