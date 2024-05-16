6033f791a8f8ea14c942d80a1b7ff0fc076a7a1a
Vamcharivilas <vamshiharivilas1112@gmail.com>
2024-05-16 15:18:27 +0530

    Add files via upload

---
+++ lab4/AreaOfCircle.java
@@ -0,0 +1,32 @@
+package functions;
+
+import java.util.Scanner;
+
+public class AreaOfCircle {
+         
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		 String userInput;
+
+	        do{
+	            System.out.println("Do you want to find the area of a circle? YES(press enter)");
+	            userInput = sc.nextLine().toUpperCase(); // Convert input to uppercase for case-insensitive comparison
+	        } 
+	        while((userInput.equals("YES")));
+	        System.out.println("enter the value of radius r is :");
+	  	  double r = sc.nextDouble();
+
+	         System.out.println(	  areaOfCircle(r));
+
+	  	}
+	      public static double areaOfCircle( double r){
+	      	Double pi = 3.14;
+	            double a=pi*r*r;
+	  		return a;
+	      	
+	      }
+		
+	
+
+}
---
+++ lab4/UserWelcome.java
@@ -0,0 +1,19 @@
+package functions;
+
+import java.util.Scanner;
+
+public class UserWelcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter your Name :");
+		String Name=sc.nextLine();
+		sc.close();
+		String WelcomeMessage=" YOUR MOST Welcome"+Name+"!";
+		System.out.println(WelcomeMessage);
+		
+
+	}
+
+}
