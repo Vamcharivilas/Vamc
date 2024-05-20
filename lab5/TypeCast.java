1d182f78ae2d0d73228d45d4b5f22b09c213600a
Vamcharivilas <vamshiharivilas1112@gmail.com>
2024-05-20 15:08:17 +0530

    Add files via upload

---
+++ lab5/TypeCast.java
@@ -0,0 +1,17 @@
+package field;
+
+public class TypeCast {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //Narrow 
+		int i = 10;
+		long l = (long)i;
+		System.out.println(l);
+		double d = (double)i;
+		System.out.println(d);
+		float f = (float)i;
+		System.out.println(f);
+	}
+
+}
